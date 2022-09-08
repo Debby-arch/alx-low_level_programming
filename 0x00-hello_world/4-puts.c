9360c2625259802cd16dd5178bc2d096b1b18737
Debby-arch <opiyorosedebrah@gmail.com>
2022-09-08 14:42:09 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main-this is the main function
+ *
+ * return: 0
+ */
+int main(void)
+{
+	/*printing out string using puts*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
