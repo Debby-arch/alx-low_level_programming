d3dbcc9fbb6be1f933dcdd59ce09f24a93422e8a
Debby-arch <opiyorosedebrah@gmail.com>
2022-09-08 16:42:10 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
- * main-this is the main function
- *
- * return: 0
- */
+
+/**
+ * main - main block
+ * Return: 0
+  */
+
 int main(void)
 {
-	/*printing out string using puts*/
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+ puts("\"Programming is like building a multilingual puzzle");
+ Return (0);
 }
